94690abc654c174bf9f49231be7bf78575385df3
kms040224 <kms0402246103@gmail.com>
2024-05-28 17:54:57 +0900

    feat : 배열 공부
    집가서 에외처리 적용해서 한번 나온 게임은 안나오게 만들어볼 예정

---
+++ Cording Practice/Assets/Scripts/0528/array.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class array : MonoBehaviour
+{
+
+    //배열은 모든 타입의 값을 저장 가능(이때 모든 요소는 동일한 타입)
+    //배열을 생성 시 길이나 요소의 개수를 설정 가능 (추후에 변경이 불가능하다(?))
+    //배열을 생성할 때 초기값을 지정하지 않으면 기본값이 지정된다. integer와 같은 숫자 변수형의 경우에는 0으로 자동 설정되고 다른 타입은 null값이나 미설정 상태가 된다.
+
+    void Start()
+    {
+        // *배열 생성시 필요한것 -> 요소의 타입(변수형 등) 과 대괄호 쌍,고유한 이름이 필요하다.
+        //new 키워드의 경우 메모리에 배열을 생성하는 데 사용되며 값 타입과 대괄호 쌍이 뒤에 따라온다.배열이 저장할 요소의 개수는 두번째 대괄호 쌍 안에 들어간다.
+
+        //일반 초기화
+        //string[] poFavoriteGame = new string[] { "원신", "붕괴 스타레일", "블루 아카이브","롤" };
+
+        //단축 초기화
+        //string[] poFavoriteGame = { "원신", "붕괴 스타레일", "블루 아카이브","롤" };
+
+       
+    }
+
+    public void poTodaysGame()
+    {
+        string[] poFavoriteGame = new string[] { "원신", "붕괴 스타레일", "블루 아카이브", "롤" };
+        int randomNum = Random.Range(0, 4);
+
+        Debug.Log("오늘의 게임은 : " + poFavoriteGame[randomNum]);
+
+        //전에 나왔던거 안나오게 바꿔볼 것
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            poTodaysGame();
+        }
+        
+        
+    }
+}
