5e97a61b3961808bf500c4bb2bba8ef71be23eef
kms040224 <kms0402246103@gmail.com>
2024-06-09 23:32:45 +0900

    feat : 맹독에 걸린 김평오

---
+++ Cording Practice/Assets/Scripts/0609/ExWhile.cs
@@ -0,0 +1,38 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExWhile : MonoBehaviour
+{
+
+    public int turn = 0;
+    public int hp = 320;
+    public int poisonDmg = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Poison();
+    }
+
+    public void Poison()
+    {
+        Debug.Log("김평오는 독에 걸려있다");
+        poisonDmg = 40;
+        while (hp == 0) ;
+        {
+            hp = hp - poisonDmg;
+            Debug.Log(hp);
+            Debug.Log("독으로 인해 김평오의 체력이 감소했다");
+            
+        }
+        Debug.Log("김평오는 결국 쓰러졌다");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
